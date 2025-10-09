f0b140f01c392d82d83fe8c3e3da4600b908d3d8
Jack Dixon <jackaverydixon@gmail.com>
2025-10-09 13:27:11 -0400

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     die('Error reading user data.');
   }
 
-  // Search for user
   $foundUser = null;
   foreach ($users as $user) {
     if ($user['username'] === $username) {
@@ -100,3 +99,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
